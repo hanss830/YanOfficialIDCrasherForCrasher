f959cb3a940e8beacdd77c2e9c19de64b49d562d
YST STORE <kinghanz971@gmail.com>
2025-07-05 17:49:14 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,38 +1,29 @@
 #!/bin/bash
-# YanNewBotz - CrasherForCrasher Tools
-# By YanOfficialID
+
+# Warna
+W1='\033[0;31m'  # Merah
+W2='\033[1;33m'  # Kuning
+W3='\033[0;32m'  # Hijau
+W4='\033[0;34m'  # Biru
+W5='\033[1;35m'  # Ungu
+W6='\033[0;36m'  # Cyan
+W7='\033[1;37m'  # Putih
+NC='\033[0m'     # Reset
 
 clear
-# Warna
-R='\033[0;31m'
-G='\033[0;32m'
-B='\033[0;34m'
-Y='\033[1;33m'
-CY='\033[0;36m'
-P='\033[1;35m'
-NC='\033[0m' # no color
 
-echo -e "${R}████████████████████████████████████"
-echo -e "${Y}█                                  █"
-echo -e "${G}█     ${P}CrahserForCrasher${G}            █"
-echo -e "${Y}█                                  █"
-echo -e "${R}████████████████████████████████████"
+echo -e "${W1}C${W2}r${W3}a${W4}h${W5}s${W6}e${W7}r${W1}F${W2}o${W3}r${W4}C${W5}r${W6}a${W7}s${W1}h${W2}e${W3}r${NC}"
 
-echo -e "${CY}        ----|"
-echo -e "${CY}            |"
-echo -e "${G}██████ READY SYSTEM ██████"
 echo ""
-echo -e "${P}© YanOfficialID 2025"
+echo -e "${W4}██████ SYSTEM READY ██████"
 echo ""
-echo -e "${CY}YAN READY TO GET YOU TARGET 💀"
-echo ""
-echo -e "${B}[1] WhatsApp"
-echo -e "${B}[2] Telegram"
-echo -e "${B}[3] TqTo"
-echo -e "${B}[4] Owner"
+
+echo -e "${W7}[1] WhatsApp Crasher"
+echo -e "${W7}[2] Telegram Tools"
+echo -e "${W7}[3] TqTo Tools"
+echo -e "${W7}[4] Owner Info"
 echo ""
 read -p "Pilih Menu: " menu
-
 case $menu in
     1)
         echo -e "${G}Fitur WhatsApp Crasher Segera Hadir..."
