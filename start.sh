e1e68bf25ad26f72e6c98def76b61a6a40b89800
YST STORE <kinghanz971@gmail.com>
2025-07-05 17:44:06 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# YanNewBotz - CrasherForCrasher Tools
+# By YanOfficialID
+
+clear
+# Warna
+R='\033[0;31m'
+G='\033[0;32m'
+B='\033[0;34m'
+Y='\033[1;33m'
+CY='\033[0;36m'
+P='\033[1;35m'
+NC='\033[0m' # no color
+
+echo -e "${R}████████████████████████████████████"
+echo -e "${Y}█                                  █"
+echo -e "${G}█     ${P}CrahserForCrasher${G}            █"
+echo -e "${Y}█                                  █"
+echo -e "${R}████████████████████████████████████"
+
+echo -e "${CY}        ----|"
+echo -e "${CY}            |"
+echo -e "${G}██████ READY SYSTEM ██████"
+echo ""
+echo -e "${P}© YanOfficialID 2025"
+echo ""
+echo -e "${CY}YAN READY TO GET YOU TARGET 💀"
+echo ""
+echo -e "${B}[1] WhatsApp"
+echo -e "${B}[2] Telegram"
+echo -e "${B}[3] TqTo"
+echo -e "${B}[4] Owner"
+echo ""
+read -p "Pilih Menu: " menu
+
+case $menu in
+    1)
+        echo -e "${G}Fitur WhatsApp Crasher Segera Hadir..."
+        ;;
+    2)
+        echo -e "${G}Fitur Telegram Segera Hadir..."
+        ;;
+    3)
+        echo -e "${G}TqTo - Thank You Tools Segera Hadir..."
+        ;;
+    4)
+        echo -e "${G}Owner: wa.me/601117880078"
+        ;;
+    *)
+        echo -e "${R}Pilihan Tidak Ada!"
+        ;;
+esac
